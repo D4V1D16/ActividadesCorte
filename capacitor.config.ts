41b19cc714ec6cad4dfec5ac22380254435b9476
Disruptor <davidsantiagojimenez16@gmail.com>
2024-05-03 08:40:21 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Corte2Activites',
+  webDir: 'www'
+};
+
+export default config;
